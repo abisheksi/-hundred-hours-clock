fb887eab1575d1717e05d971721486c89ad001a4
Abishek Sitaula <abisheksitaula07@gmail.com>
2026-08-14 19:21:36 +0545

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,31 @@
+rootProject.name = "hundred-hours-clock"
+
+pluginManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+    }
+}
+
+include(":androidApp")
+include(":shared")
